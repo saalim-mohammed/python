515f3dc960dbe7db2379f8e70ea06fd8095aa448
saalimpythn <saalimmohammed555@gmail.com>
2020-01-14 15:21:27 +0530

    commit2

---
+++ operators/arithmeticoperators.py
@@ -0,0 +1,19 @@
+no1=10
+no2=20
+result=no1+no2
+print("additional result=",result)
+result=no2-no1
+print("subtraction result=",result)
+result=no1*no2
+print("multipply result=",result)
+result=no2/no1
+print("division result=",result)
+result=no2%no1
+print("% result=",result)
+result=(no1**no2)
+print("exponentiation result",result)
+result=(no1//no2)
+print("floor division result",result)
+
+
+
